63203df53f7e01d4ee8867111d3d8a18e67c1822
yonatan kreiner <yonatanKreiner@gmail.com>
2017-09-28 20:52:34 +0300

    added bootstrap and google analytics

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -48,6 +48,7 @@ export default {
 			inject: 'head',
 			// Properties defined here are available in index.html
 			// using htmlWebpackPlugin.options.varName
+			googleAnalyticsToken: 'UA-104454728-2',
 			trackJSToken: 'c052939146f74fefbd3b4564797c0b10'
 		}),
 
