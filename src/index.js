ce6fc92393090d673337676bf6846f559f92e894
yonatan kreiner <yonatanKreiner@gmail.com>
2017-09-30 14:24:39 +0300

    rearrange index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import 'angular';
 import './index.css'
+
+import angular from 'angular';
+import uirouter from 'angular-ui-router';
+
+import routing from './index.config';
+
+angular.module('arrowfind', [uirouter])
+	.config(routing);
