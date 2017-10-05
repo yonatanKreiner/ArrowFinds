7d228953539780bdc964dd14c60020cca62cffe1
yonatan kreiner <yonatanKreiner@gmail.com>
2017-10-05 17:28:41 +0300

    fixed conflict

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -3,14 +3,9 @@ import './landing.css';
 import angular from 'angular';
 
 import landing from './landing.directive';
-import features from './features';
-<<<<<<< HEAD
 import home from './home';
-
-export default angular.module('arrowfind.landing', [landing, features,home])
-=======
+import features from './features';
 import pricing from './pricing';
 
-export default angular.module('arrowfind.landing', [landing, features, pricing])
->>>>>>> 741255970fafeaf24506bd93d04be33c75d14c51
+export default angular.module('arrowfind.landing', [landing, home, features, pricing])
   .name;
