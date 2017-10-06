c9dab23ea6767854a0f70e6f56fab244d2b3cad9
iuyt9003 <iuyt9003@gmail.com>
2017-10-06 15:51:23 +0300

    move the controller to a diffrenet file.

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,56 +1,8 @@
-// import angular from 'angular';
-// import SweetAlert from 'sweetalert';
+import './login.css';
 
+import angular from 'angular';
 
-// angular.module('arrowfind', [uirouter, navbar, landing,login]).controller('loginController', ['$rootScope','$scope','$http','SweetAlert','$window',function ($rootScope,$scope,$http, SweetAlert,$window) {
+import loginController from './login.controller';
 
-//     $scope.msgLogin = false;
-
-//     $scope.createuser = function()
-//     {
-//         $http.get('/api/CreateUser', {
-//             params:  {user: $scope.email,password: $scope.password},
-//             headers: {}
-//         }
-//         )
-//         .then(function(response) {
-//         // Request completed successfully
-//             if(response.data == "true")
-//             {
-//                 window.swal("כל הכבוד","היוזר " + $scope.email+" נוצר בהצלחה", "success");
-//                 $window.location.href = '/#!/monitor';
-//             }
-//             else
-//             {
-//                  window.swal("היי היוזר כבר קיים","אנא תבחרו יוזר אחר", "error");
-//             }
-            
-//         }, function(x) {
-//         // Request error
-//         });
-//     };
-//     $scope.login = function()
-//     {
-//         $http.get('/api/Login', {
-//             params:  {user: $scope.email,password: $scope.password},
-//             headers: {}
-//         }
-//         )
-//         .then(function(response) {
-//         // Request completed successfully
-//             if(response.data == "true")
-//             {
-
-//                 $window.location.href = '/#!/monitor';
-//             }
-//             else
-//             {
-//                   $scope.msgLogin = true;
-//             }
-            
-//         }, function(x) {
-//         // Request error
-//         });
-//     };
-
-// }]);
+angular.module('arrowfind', [])
+    .controller('loginController', loginController);
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import uirouter from '@uirouter/angularjs';
 import routing from './index.config';
 import navbar from './components/navbar';
 import landing from './components/landing';
+import login from './components/login';
 
 angular.module('arrowfind', [uirouter, navbar, landing])
 	.config(routing);
