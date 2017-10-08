77e8b7c97cb0125b1fd2ffe1d1271afe82017c92
iuyt9003 <iuyt9003@gmail.com>
2017-10-08 04:11:28 +0300

    fixed login html , added login css , added to controller the api calls (working) - need to install sweetalert to this , and enable cors on the server to allow our app to make an api call

--- src/components/login/login.controller.js
+++ src/components/login/login.controller.js
@@ -1,12 +1,52 @@
 export default class LoginController {
-  constructor() {
-    this.name = 'World';
+  constructor(http,window) {
+    this.msgLogin = false;
+		this.urlTest = 'http://127.0.0.1:5000';
+		this.$http = http;
+		this.$window = window;
   }
 
-  changeName() {
-    this.name = 'angular-tips';
-  }
+	createuser(){
+		this.$http.get(this.urlTest + '/api/CreateUser', {
+				params: {user: this.email,password: this.password},
+				headers: {}
+		}).then(function(response) {
+			// Request completed successfully
+			if(response.data == "true")
+			{
+					// SweetAlert Msg User-Welcome
+					this.$window.location.href = '/#!/monitor';
+			}
+			else
+			{
+			// SweetAlert Error;
+			}
+		}, function(x) {
+			alert(x);
+		});
+	}
+
+	login(){
+		this.$http.get(this.urlTest + '/api/Login', {
+			params: {user: this.email,password: this.password},
+			headers: {}
+		}).then(function(response) {
+			if(response.data == "true")
+			{
+					this.$window.location.href = '/#!/monitor';
+			}
+			else
+			{
+					this.msgLogin = true;
+			}   
+		}, function(x) {
+			alert(x);
+		});
+	}
+
 }
+
+LoginController.$inject = ['$http','$window'];
 
 
 
