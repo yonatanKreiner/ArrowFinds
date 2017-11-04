af57502ea3d85ee16c1dabfdf8a6d0a96cf92b37
yonatan kreiner <yonatanKreiner@gmail.com>
2017-11-04 16:47:16 +0200

    Adding deployment server

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -6,7 +6,7 @@ import helmet from 'helmet'
 
 /* eslint-disable no-console */
 
-const port = 3000
+const port = process.env.PORT || 3000;
 const app = express()
 
 app.use(helmet())
---
+++ dist/server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var compression = require('compression');
+var helmet = require('helmet');
+
+var port = process.env.PORT || 5000;
+var app = express();
+
+app.use(helmet())
+app.use(compression())
+app.use(express.static(__dirname));
+
+app.get('/*', (req, res) => 	
+	res.sendFile(path.join(__dirname, '../dist/index.html'))
+)
+
+app.listen(port);
