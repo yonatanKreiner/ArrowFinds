c0d36de26f4572725c8b32de78f0fee329e39965
yonatan kreiner <yonatanKreiner@gmail.com>
2017-11-15 21:09:02 +0200

    after optimization

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -1,4 +1,3 @@
-import $ from 'jquery';
 export default class NavbarController {
   constructor($location, $anchorScroll, $http) {
 		this.location = $location;
@@ -10,28 +9,10 @@ export default class NavbarController {
 
   gotoAnchor(id) {
 		if (this.location.hash() !== id) {
-			// this.location.hash(id);
-			$('html, body').stop().animate({
-				scrollTop: $('#' + id).offset().top
-			}, 1500, 'easeInOutExpo');
-			event.preventDefault();
+			this.location.hash(id);
 		} else {
-			// this.scroll();
-			$('html, body').stop().animate({
-					scrollTop: $('#' + id).offset().top
-			}, 1500, 'easeInOutExpo');
-			event.preventDefault();
+			this.scroll();
 		}
-		// $(document).ready(function() {
-		// 	let $anchor = $(this);
-
-		// 	if($($anchor.attr('href')).offset().top != null) {
-		// 		$('html, body').stop().animate({
-		// 				scrollTop: $($anchor.attr('href')).offset().top
-		// 		}, 1500, 'easeInOutExpo');
-		// 		event.preventDefault();
-		// 	}
-		// })
 	}
 
 	login() {
