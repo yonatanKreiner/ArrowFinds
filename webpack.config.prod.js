85626db27181a93641a92c8a0b05d3c853f293a3
yonatan kreiner <yonatanKreiner@gmail.com>
2018-03-31 17:27:35 +0300

    update webpack prod remove uglifyJS

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -76,25 +76,6 @@ module.exports =  {
 
 		new ScriptExtHtmlWebpackPlugin({
 			defaultAttribute: 'defer'
-		}),
-
-		// Minify JS
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false,
-				screw_ie8: true,
-				conditionals: true,
-				unused: true,
-				comparisons: true,
-				sequences: true,
-				dead_code: true,
-				evaluate: true,
-				if_return: true,
-				join_vars: true
-			},
-			output: {
-				comments: false
-			}
 		})
 	],
 	module: {
